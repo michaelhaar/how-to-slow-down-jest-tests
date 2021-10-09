a188b300d26209e64e87f71b8a30c65625ac9fc2
michaelhaar <michael.haar92@gmail.com>
2021-10-09 22:55:52 +0200

    added delay to test files

--- src/experiment_a/test_a.test.js
+++ src/experiment_a/test_a.test.js
@@ -1,5 +1,14 @@
 const { addNumbers } = require('./source_a');
 
+function sleep(time) {
+  var stop = new Date().getTime();
+  while (new Date().getTime() < stop + time) {}
+}
+
+it('should run 5sec', () => {
+  sleep(1000);
+});
+
 it('should add numbers 1 to 100', () => {
   const result = addNumbers();
   expect(result).toBe(5050);
